732e0365bdd00c8cfdb80cb4123e432d0ca63b2d
Miruthula <smiruthularaman@gmail.com>
2025-09-17 09:19:09 +0530

    first commit

---
+++ src/main/java/com/mystore/mystoremiru/MystoremiruApplication.java
@@ -0,0 +1,13 @@
+package com.mystore.mystoremiru;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MystoremiruApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MystoremiruApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mystore/mystoremiru/MystoremiruApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mystore.mystoremiru;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MystoremiruApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
